b04aaa39cc32b78e327943e45a487a1b22857ae2
Daan Sieben <daan.sieben@philips.com>
2023-12-09 09:25:28 +0100

    feat: Day 09

--- solutions/src/days/day_09.rs
+++ solutions/src/days/day_09.rs
@@ -5,12 +5,22 @@ use common::Answer;
 pub struct Puzzle;
 
 impl Solution for Puzzle {
-    fn solve_a(&mut self, _input: String) -> Result<Answer, String> {
-        Answer::from("").into()
+    fn solve_a(&mut self, input: String) -> Result<Answer, String> {
+        let history = parse_input(input);
+        let total = history
+            .into_iter()
+            .map(|v| v.last().unwrap() + extrapolate(&v).1)
+            .sum::<isize>();
+        Answer::from(total).into()
     }
 
-    fn solve_b(&mut self, _input: String) -> Result<Answer, String> {
-        Answer::from("").into()
+    fn solve_b(&mut self, input: String) -> Result<Answer, String> {
+        let history = parse_input(input);
+        let total = history
+            .into_iter()
+            .map(|v: Vec<isize>| v.first().unwrap() - extrapolate(&v).0)
+            .sum::<isize>();
+        Answer::from(total).into()
     }
 
     #[cfg(feature = "ui")]
@@ -19,20 +29,45 @@ impl Solution for Puzzle {
     }
 }
 
+fn parse_input(input: String) -> Vec<Vec<isize>> {
+    input
+        .lines()
+        .map(|line| {
+            line.split_ascii_whitespace()
+                .map(|d| d.parse::<isize>().unwrap())
+                .collect::<Vec<isize>>()
+        })
+        .collect()
+}
+
+fn extrapolate(history: &[isize]) -> (isize, isize) {
+    if history.iter().all(|n| n == &0_isize) {
+        return (0, 0);
+    }
+    let deltas: Vec<isize> = history.windows(2).map(|w| w[1] - w[0]).collect();
+    let extrapolated = extrapolate(&deltas);
+    (
+        deltas.first().unwrap() - extrapolated.0,
+        deltas.last().unwrap() + extrapolated.1
+    )
+}
+
 #[cfg(test)]
 mod tests {
     use super::Puzzle;
     use super::Solution;
     use common::Answer;
 
-    const TEST_INPUT: &str = "";
+    const TEST_INPUT: &str = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
 
     #[tokio::test]
     async fn part_a() {
         let mut puzzle = Puzzle::default();
         assert_eq!(
             puzzle.solve_a(String::from(TEST_INPUT)),
-            Ok(Answer::from(""))
+            Ok(Answer::from(114))
         )
     }
 
@@ -41,7 +76,7 @@ mod tests {
         let mut puzzle = Puzzle::default();
         assert_eq!(
             puzzle.solve_b(String::from(TEST_INPUT)),
-            Ok(Answer::from(""))
+            Ok(Answer::from(2))
         )
     }
 }
