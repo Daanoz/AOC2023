3df99fedbda9b5d307067c411587d8eb9ea83f28
Daan Sieben <daan.sieben@philips.com>
2023-12-11 18:44:06 +0100

    feat: Day 11

--- solutions/src/days/day_11.rs
+++ solutions/src/days/day_11.rs
@@ -1,16 +1,31 @@
 use super::Solution;
 use common::Answer;
 
-#[derive(Default)]
-pub struct Puzzle;
+pub struct Puzzle {
+    part_b_grow_size: usize,
+}
+
+impl Default for Puzzle {
+    fn default() -> Self {
+        Self {
+            part_b_grow_size: 1000000,
+        }
+    }
+}
+
+type Coord = (usize, usize);
 
 impl Solution for Puzzle {
-    fn solve_a(&mut self, _input: String) -> Result<Answer, String> {
-        Answer::from("").into()
+    fn solve_a(&mut self, input: String) -> Result<Answer, String> {
+        let coords = parse_input(input, 2);
+        let pairs = create_pairs(coords);
+        Answer::from(pairs.iter().map(get_distance).sum::<usize>()).into()
     }
 
-    fn solve_b(&mut self, _input: String) -> Result<Answer, String> {
-        Answer::from("").into()
+    fn solve_b(&mut self, input: String) -> Result<Answer, String> {
+        let coords = parse_input(input, self.part_b_grow_size);
+        let pairs = create_pairs(coords);
+        Answer::from(pairs.iter().map(get_distance).sum::<usize>()).into()
     }
 
     #[cfg(feature = "ui")]
@@ -19,29 +34,94 @@ impl Solution for Puzzle {
     }
 }
 
+fn parse_input(input: String, grow_size: usize) -> Vec<Coord> {
+    let grow_size = grow_size - 1;
+    let row_length = input.find('\n').unwrap();
+    let rows: Vec<usize> = input
+        .lines()
+        .enumerate()
+        .filter_map(|(y, line)| if !line.contains('#') { Some(y) } else { None })
+        .collect();
+    let cols: Vec<usize> = (0..row_length)
+        .filter(|x| {
+            !input
+                .lines()
+                .map(|l| l.chars().nth(*x).unwrap())
+                .collect::<String>()
+                .contains('#')
+        })
+        .collect();
+    let coords = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, l)| {
+            l.chars()
+                .enumerate()
+                .filter_map(|(x, c)| {
+                    if c == '#' {
+                        let row_count = rows.iter().filter(|r| **r < y).count();
+                        let col_count = cols.iter().filter(|r| **r < x).count();
+                        Some((x + (col_count * grow_size), y + (row_count * grow_size)))
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<Coord>>()
+        })
+        .collect::<Vec<Coord>>();
+    coords
+}
+
+fn create_pairs(coords: Vec<Coord>) -> Vec<(Coord, Coord)> {
+    let mut pairs = vec![];
+    for (i, coord) in coords.iter().enumerate() {
+        for other in coords.iter().skip(i + 1) {
+            pairs.push((*coord, *other));
+        }
+    }
+    pairs
+}
+
+fn get_distance(pair: &(Coord, Coord)) -> usize {
+    let ((x1, y1), (x2, y2)) = pair;
+    let x = (*x1 as isize - *x2 as isize).unsigned_abs();
+    let y = (*y1 as isize - *y2 as isize).unsigned_abs();
+    x + y
+}
+
 #[cfg(test)]
 mod tests {
     use super::Puzzle;
     use super::Solution;
     use common::Answer;
 
-    const TEST_INPUT: &str = "";
+    const TEST_INPUT: &str = "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....";
 
     #[tokio::test]
     async fn part_a() {
         let mut puzzle = Puzzle::default();
         assert_eq!(
             puzzle.solve_a(String::from(TEST_INPUT)),
-            Ok(Answer::from(""))
+            Ok(Answer::from(374))
         )
     }
 
     #[tokio::test]
     async fn part_b() {
         let mut puzzle = Puzzle::default();
+        puzzle.part_b_grow_size = 10;
         assert_eq!(
             puzzle.solve_b(String::from(TEST_INPUT)),
-            Ok(Answer::from(""))
+            Ok(Answer::from(1030))
         )
     }
 }
